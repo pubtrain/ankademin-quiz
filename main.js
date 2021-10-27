762b5c09c6f61d47c22a080bd66ab989436363f2
pubtrain <kallesandholm@gmail.com>
2021-10-27 14:59:56 +0200

    Added and rebuilt html, added some js

--- main.js
+++ main.js
@@ -5,8 +5,35 @@ darkButton.addEventListener("click", () => {
   toggleDark.classList.toggle("dark-mode");
 })
 
-let restartButton = document.querySelector("#restart")
+let restartButton = document.querySelector("#restart");
 
 restartButton.addEventListener("click", () => {
   let restartGame = location.reload();
 })
+
+let questionOne = document.querySelectorAll("[name='question1']");
+let questionTwo = document.querySelectorAll("[name='question2']");
+let questionThree = document.querySelectorAll("[name='question3']");
+let questionFour = document.querySelectorAll("[name='question4']");
+let questionFive = document.querySelectorAll("[name='question5']");
+let questionSix = document.querySelectorAll("[name='question6']");
+let questionSeven = document.querySelectorAll("[name='question7']");
+let questionEight = document.querySelectorAll("[name='question8']");
+let questionTen = document.querySelectorAll("[name='question10']");
+let answerOne = "";
+let answerTwo = "";
+let answerThree = "";
+let answerFour = "";
+let answerFive = "";
+let answerSix = "";
+let answerSeven = "";
+let answerEight = "";
+let answerTen = "";
+
+
+
+
+
+
+
+
